9c54d9f360b981f607c22725a7d1a9340670ce12
zhangchunming <mrspring@126.com>
2015-10-06 15:48:34 +0800

    filter

---
+++ src/main/java/com/ichunming/filter/CharsetEncodingFilter.java
@@ -0,0 +1,50 @@
+/**
+ * charset encoding filter
+ * 2015/10/06
+ * chunming
+ */
+package com.ichunming.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class CharsetEncodingFilter
+ */
+public class CharsetEncodingFilter implements Filter {
+	
+	private String encodeString;
+	
+    /**
+     * Default constructor. 
+     */
+    public CharsetEncodingFilter() {}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding(encodeString);
+		response.setCharacterEncoding(encodeString);
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		encodeString=fConfig.getInitParameter("encoding");
+	}
+
+}
