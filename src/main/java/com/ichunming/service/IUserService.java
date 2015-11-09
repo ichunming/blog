30aa752db5941e1035497219b6e3ba3ac187a24f
zhangchunming <407201244@qq.com>
2015-11-09 23:01:34 +0800

    test use src del

--- src/main/java/com/ichunming/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.ichunming.service;
-
-import java.util.List;
-import com.ichunming.bean.User;
-
-public interface IUserService {
-	public List<User> getAllUser();
-	public void deleteUpdateUser(int id, User user);
-}
